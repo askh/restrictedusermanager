79a3be83e5928cd7dc1bb01bcb171972ad37439d
Alexander Kharitonov <askh@askh.ru>
2021-11-29 00:52:55 +0300

    First commit.

---
+++ src/restrictedusermanager.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <boost/optional.hpp>
+#include <boost/program_options.hpp>
+#include <boost/program_options/option.hpp>
+#include <boost/program_options/options_description.hpp>
+#include <yaml-cpp/yaml.h>
+
+namespace po = boost::program_options;
+
+const std::string CONFIG_FILE_NAME = "restrictedusermanager.cpp";
+const std::string CONFIG_DIRS[] = {"/usr/local/etc", "/etc"};
+
+int main(int argc, char **argv) {
+	po::options_description desc("Available options");
+	desc.add_options()
+        ("help,h", "Show help.")
+		("add-user,a", po::value<std::string>(), "Add user");
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);
+
+    if(vm.count("help")) {
+    	std::cout << desc << "\n";
+    	return 0;
+    }
+
+    if(vm.count("add-user")) {
+    	std::string user_name = vm["add-user"].as<std::string>();
+    	std::cout << user_name << "\n";
+    }
+
+	return 0;
+}
